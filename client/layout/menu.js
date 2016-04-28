0d2fb838765fc650b6d8eee1ea3b0a17ccde90ba
baiyalong <baiyalong@gmail.com>
2016-04-27 23:51:44 -0400

    menu

--- client/layout/menu.js
+++ client/layout/menu.js
@@ -9,9 +9,62 @@ Template.menu.onRendered(() => {
 })
 
 Template.menu.helpers({
-
+    menus: () => {
+        return [
+            // {
+            //     icon: 'fa fa-dashboard',
+            //     text: '仪表盘',
+            //     route: '/dashboard'
+            // },
+            {
+                icon: 'fa fa-dashboard',
+                text: '监测数据',
+                route: '/dashboard'
+            },
+            {
+                icon: 'fa fa-dashboard',
+                text: '预报数据',
+                route: '/dashboard'
+            },
+            {
+                icon: 'fa fa-dashboard',
+                text: '预警信息',
+                route: '/dashboard'
+            },
+            {
+                icon: 'fa fa-dashboard',
+                text: '服务监控',
+                route: '/dashboard'
+            },
+            {
+                icon: 'fa fa-gears',
+                text: '配置数据',
+                route: '/dashboard'
+            },
+            {
+                icon: 'fa fa-users',
+                text: '用户管理',
+                route: '/dashboard'
+            },
+            {
+                icon: 'fa fa-dashboard',
+                text: '用户反馈',
+                route: '/dashboard'
+            },
+            {
+                icon: 'fa fa-weixin',
+                text: '微信',
+                route: '/dashboard'
+            },
+            {
+                icon: 'fa fa-weibo',
+                text: '微博',
+                route: '/dashboard'
+            },
+        ]
+    }
 })
 
 Template.menu.events({
-    
+
 })
